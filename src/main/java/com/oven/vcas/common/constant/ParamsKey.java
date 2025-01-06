676e294983de15a5a2db5c5da4117988362bc0c5
Oven <503612012@qq.com>
2025-01-06 08:58:16 +0800

    配置文件&参数key常量

---
+++ src/main/java/com/oven/vcas/common/constant/ParamsKey.java
@@ -0,0 +1,15 @@
+package com.oven.vcas.common.constant;
+
+/**
+ * 参数key常量
+ *
+ * @author Oven
+ */
+public interface ParamsKey {
+
+    // 告警类型
+    String ALARM_TYPE = "alarm_type";
+    // 模型阈值
+    String MODEL_THRESHOLD = "model_threshold";
+
+}
